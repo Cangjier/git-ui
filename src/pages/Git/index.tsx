9a911cad23722c88a2afbdd6ef73fc25f2eeceef
Cangjier <moodlee@qq.com>
2025-09-19 17:57:41 +0800

    Update layout.json to replace Translate tab with Swig; enhance Git component with project and branch name inputs, and a commit message field

--- src/pages/Git/index.tsx
+++ src/pages/Git/index.tsx
@@ -1,12 +1,39 @@
-import { forwardRef } from "react";
+import { Button, Input } from "antd";
+import { forwardRef, useState } from "react";
+import { RedoOutlined } from "@ant-design/icons";
 
 export const Git = forwardRef<HTMLDivElement, {}>((props, ref) => {
+    const [projectName, updateProjectName] = useState("");
+    const [branchName, updateBranchName] = useState("");
+    const [message, updateMessage] = useState("");
     return <div ref={ref} style={{
         display: "flex",
         flexDirection: "column",
-        flex: 1,
-        backgroundColor: "#fff"
+        backgroundColor: "#fff",
+        gap: "5px"
     }}>
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "5px"
+        }}>
+            <div>{projectName}</div>
+            <div>{branchName}</div>
+            <div style={{
+                flex: 1
+            }}>
 
+            </div>
+            <Button type="text" icon={<RedoOutlined />} />
+        </div>
+        {/* Commit Message */}
+        <Input placeholder="Message" value={message} onChange={(e) => updateMessage(e.target.value)} />
+        {/* Changes */}
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            overflowY: "auto"
+        }}></div>
     </div>
 });
